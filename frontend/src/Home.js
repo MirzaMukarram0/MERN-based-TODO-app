20852f63386cf07fd40d2436570a5a69b820b963
MirzaMukarram0 <ahmedmukarram6@gmail.com>
2025-05-01 18:15:25 +0500

    Deletion logic added

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -2,13 +2,14 @@ import React, { useEffect, useState } from 'react';
 import Create from './Create';
 import './App.css';
 import axios from 'axios';
-import { BsCircleFill, BsFillCheckCircleFill, BsFillTrashFill, BsPencil, BsCheckLg, BsClock } from 'react-icons/bs';
+import { BsCircleFill, BsFillCheckCircleFill, BsFillTrashFill, BsPencil, BsCheckLg, BsClock, BsX } from 'react-icons/bs';
 import { formatDistanceToNow } from 'date-fns';
 
 const Home = () => {
     const [todos, setTodos] = useState([]);
     const [updatetask, setUpdatetask] = useState('');
     const [taskid, setTaskid] = useState('');
+    const [deleteDialog, setDeleteDialog] = useState({ show: false, id: null, task: '' });
 
     useEffect(() => {
         axios.get('http://localhost:5000/get')
@@ -49,12 +50,21 @@ const Home = () => {
             .catch(err => console.log(err));
     };
 
+    const showDeleteDialog = (id, task) => {
+        setDeleteDialog({ show: true, id, task });
+    };
+
+    const hideDeleteDialog = () => {
+        setDeleteDialog({ show: false, id: null, task: '' });
+    };
+
     const Hdelete = (id) => {
         axios.delete(`http://localhost:5000/delete/${id}`)
             .then(result => {
                 console.log(result.data);
                 const updatedTodos = todos.filter(todo => todo._id !== id);
                 setTodos(updatedTodos);
+                hideDeleteDialog();
             })
             .catch(err => console.log(err));
     };
@@ -111,12 +121,43 @@ const Home = () => {
                                             }} 
                                         />
                                     )}
-                                    <BsFillTrashFill className='icon delete-icon' onClick={() => Hdelete(todo._id)} />
+                                    <BsFillTrashFill 
+                                        className='icon delete-icon' 
+                                        onClick={() => showDeleteDialog(todo._id, todo.task)} 
+                                    />
                                 </span>
                             </div>
                         </div>
                     ))
             }
+
+            {/* Delete Confirmation Dialog */}
+            {deleteDialog.show && (
+                <div className="dialog-overlay">
+                    <div className="dialog-content">
+                        <button className="dialog-close" onClick={hideDeleteDialog}>
+                            <BsX />
+                        </button>
+                        <h3>Delete Task</h3>
+                        <p>Are you sure you want to delete this task?</p>
+                        <p className="task-preview">{deleteDialog.task}</p>
+                        <div className="dialog-buttons">
+                            <button 
+                                className="dialog-button cancel-button" 
+                                onClick={hideDeleteDialog}
+                            >
+                                Cancel
+                            </button>
+                            <button 
+                                className="dialog-button delete-button" 
+                                onClick={() => Hdelete(deleteDialog.id)}
+                            >
+                                Delete
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            )}
         </main>
     );
 };
